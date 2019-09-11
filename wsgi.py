6841d01952d1b2a502c93176642e1710a76e0768
Delhivery <oppradhan2011@gmail.com>
2019-09-12 03:36:34 +0530

    Updated requirements

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,11 @@
 from factory import initialize_app
 from flask import Flask
+import os
+
 
 application = Flask(__name__, instance_relative_config=True)
 initialize_app(application)
 
 if __name__ == '__main__':
-    application.run()
+    port = int(os.environ.get('PORT', 5000))
+    application.run(host='0.0.0.0', port=port)
