bbd5e22af8131251db20880cad28315c10d33243
Delhivery <oppradhan2011@gmail.com>
2019-09-12 03:42:44 +0530

    Updated requirements

--- wsgi.py
+++ wsgi.py
@@ -7,5 +7,4 @@ application = Flask(__name__, instance_relative_config=True)
 initialize_app(application)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    application.run(host='0.0.0.0', port=port)
+    application.run(host='0.0.0.0')
